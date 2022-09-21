84bce9935f7a4242978722c2c2dfd079e8c71d71
Giovani Silva <giovani.silva@live.com>
2022-09-21 20:01:30 -0300

    pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { Home } from './pages/Home';
+import { Settings } from './pages/Settings';
+import { Transactions } from './pages/Transactions';
+import { Wallets } from './pages/Wallets';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/transactions" element={<Home />} />
-        <Route path="/wallets" element={<Home />} />
-        <Route path="/settings" element={<Home />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/wallets" element={<Wallets />} />
+        <Route path="/settings" element={<Settings />} />
       </Route>
     </Routes>
   );
