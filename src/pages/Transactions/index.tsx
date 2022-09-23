f41f80334b87599ed8253465295c3c9fb6546e43
Giovani Silva <giovani.silva@live.com>
2022-09-23 18:13:10 -0300

    Modal de transação

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,18 @@
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../../components/NewTransactionModal';
 import { TransactionsList } from '../../components/TransactionsList';
+import { NewTransactionButton, TransactionsWrapper } from './styles';
 
 export function Transactions() {
-  return <TransactionsList />;
+  return (
+    <TransactionsWrapper>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <NewTransactionButton>Nova transação</NewTransactionButton>
+        </Dialog.Trigger>
+        <NewTransactionModal />
+      </Dialog.Root>
+      <TransactionsList />
+    </TransactionsWrapper>
+  );
 }
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const NewTransactionButton = styled.button`
+  height: 50px;
+  border: 0;
+  background: ${(props) => props.theme['background-300']};
+  color: ${(props) => props.theme['text-500']};
+  font-weight: bold;
+  padding: 0 1.25rem;
+  border-radius: 6px;
+  margin-top: 1.25rem;
+  margin-left: auto;
+  cursor: pointer;
+  &:hover {
+    background: ${(props) => props.theme['primary-500']};
+    transition: background-color 0.2s;
+  }
+`;
+
+export const TransactionsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+`;
