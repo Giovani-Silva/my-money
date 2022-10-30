27ce9be1aba22ad183ab25a53f8bf19580ecb113
Giovani Silva <giovani.silva@live.com>
2022-10-30 19:51:00 -0300

    context

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,8 +1,20 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer, useState } from 'react';
 
 import defaultTheme from '../styles/themes/default';
 import darkTheme from '../styles/themes/dark';
 
-export default React.createContext({
-  theme: dark,
+
+const [theme, setTheme ] = useState(defaultTheme);
+
+const themes = { defaultTheme, darkTheme }
+
+ const toggleTheme = (theme) => {
+  setTheme({
+    theme
+  });
+ };
+const contextTheme = createContext({
+  theme,
 });
+
+export { themes, toggleTheme, contextTheme };
